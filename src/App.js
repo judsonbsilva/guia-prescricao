5f34ce8c25f1064d1456812f14c5cce6c457ee50
Judson B <judsonbarroso@gmail.com>
2021-07-26 17:17:18 -0300

    chore: change deploy mode and add site title

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import Papa from 'papaparse';
 //import MaterialTable from "material-table";
 import { useTable, usePagination, useFilters } from 'react-table';
 
-const useFetch = (url) => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(url);
-      const text = await response.text();
-      setData(text);
-    }
-    fetchData();
-  }, [url]);
-
-  return data;
-};
-
 const parseCSV = (data) => {
   return !data ? [] : Papa.parse(data, { header: true }).data.map((row) => {
     let args = {};
@@ -94,7 +79,7 @@ function App (){
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    //rows,
     prepareRow,
     pageOptions,
     page,
@@ -123,18 +108,29 @@ function App (){
   };
 
   return (
-    <div className='w-full h-full'>
-    <input
-      value={filterInput}
-      onChange={handleFilterChange}
-      placeholder={'Buscar'}
-    />
-    <table {...getTableProps()} className='table-fixed w-full'>
+    <div className='w-full h-full px-4'>
+     <header className="flex flex-wrap">
+      <h1 className="flex-auto text-xl font-semibold mt-2">Guia de Prescrição</h1>
+      <div className="w-full flex-none text-sm font-medium text-gray-500">Por Judson Barroso</div>
+      <form className="relative w-1/2">
+        <svg width="20" height="20" fill="currentColor" className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+        </svg>
+        <input
+          value={filterInput}
+          onChange={handleFilterChange}
+          placeholder={'Buscar medicamento'}
+          className='focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 bg-gray-100 my-2'
+        />  
+      </form>
+    </header>
+    
+    <table {...getTableProps()} className='table-auto w-full'>
       <thead>
         {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
+          <tr {...headerGroup.getHeaderGroupProps()} className='bg-gray-800 mar'>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()} className='px-4 py-2'>
+              <th {...column.getHeaderProps()} className='px-2 py-2 text-white h-100'>
                 {column.render('Header')}
               </th>
             ))}
@@ -161,24 +157,31 @@ function App (){
           })}
       </tbody>
       <tfoot>
-      <div className="inline-flex">
+      <div className="inline-flex py-2">
         <button
           className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
           onClick={() => previousPage()} disabled={!canPreviousPage}
         >
-          <span className="material-icons-outlined">arrow_back</span>
+          {'<'}
         </button>
         <button
           className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
           onClick={() => nextPage()} disabled={!canNextPage}
         >
-          <span className="material-icons-outlined">arrow_forward</span>
+          {'>'}
         </button>
+        <div className="relative font-medium">
+          <p className='inline-block text-gray-800 text-center align-middle py-2 px-4'>
+          {pageIndex + 1} de {pageOptions.length}
+          </p>
+        </div>
       </div>
-       <div>
-        <p>{pageIndex + 1} de {pageOptions.length}</p>
-       </div>
-       <div>Go to page:</div>
+      </tfoot>
+    </table>
+    </div>
+  );
+/*  
+<div>Go to page:</div>
        <input
          type="number"
          defaultValue={pageIndex + 1 || 1}
@@ -199,27 +202,10 @@ function App (){
            </option>
          ))}
        </select>
-      </tfoot>
-    </table>
-    </div>
-  );
-/*  const tableInstance = useTable({ columns, data })
+        
 
-  return (
-    <div className="w-screen h-screen">
-      <MaterialTable
-        columns={columns}
-        data={data}
-        title="Medicações e Preços"
-        options={{
-          paging: true
-        }}
-        onPageChange={()=>{
-          console.log(arguments);
-        }}
-      />       
-    </div>
-  );*/
+
+*/
 
 }
 
