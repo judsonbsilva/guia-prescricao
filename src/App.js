3aa74bc429f0ec17d050a2716dd69938c97fa2c8
Judson B <judsonbarroso@gmail.com>
2021-07-26 14:44:18 -0300

    featura: add pagination and filter search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse';
-import MaterialTable from "material-table";
+//import MaterialTable from "material-table";
+import { useTable, usePagination, useFilters } from 'react-table';
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
@@ -44,21 +45,165 @@ const parseCSV = (data) => {
     return { ...row, ...args };
   })
 }
-
 const columns = [
-  //{ title:'ID', field: 'id' },
-  { title:'Princípio Ativo', field: 'substancia' },
-  { title:'Concentração', field: 'concentracao' },
-  { title:'Marcas', field: 'marcas' },
-  { title:'Apresentação', field: 'apresentacao' },
-  { title:'Mínimo (R$)', field: 'minino' },
-  { title:'Máximo (R$)', field: 'maximo' },
-  { title:'Médio (R$)', field: 'mediano' }
+  { Header:'Princípio ativo', accessor: 'substancia' },
+  { Header:'Concentração', accessor: 'concentracao' },
+  { Header:'Marcas', accessor: 'marcas' },
+  { Header:'Apresentação', accessor: 'apresentacao' },
+  { Header:'Mínimo (R$)', accessor: 'minino' },
+  { Header:'Máximo (R$)', accessor: 'maximo' },
+  { Header:'Médio (R$)', accessor: 'mediano' }
 ];
 
+const pageSizeOptions = [10, 50]
+
 function App (){
-  const text = useFetch('/drugs.csv');
-  const data = parseCSV(text);
+  const [data, setData] = useState([]);
+  const [filterInput, setFilterInput] = useState('');
+  
+  useEffect(() => {
+    if(data.length == 0){
+      (async ()=> {
+        const response = await fetch('/drugs.csv');
+        const text = await response.text();
+        const data = parseCSV(text);
+        setData(data);
+      })();
+    }
+  });
+  
+  // Input element
+  /*= React.useCallback(() => {
+    const text = useFetch('/drugs.csv');
+    return parseCSV(text)
+  });*/
+
+  //const text = useFetch('/drugs.csv');
+  //const data = parseCSV(text);
+  /*const columns = React.useMemo(() => [
+    { Header:'Princípio Ativo', accessor: 'substancia' },
+    { Header:'Concentração', accessor: 'concentracao' },
+    { Header:'Marcas', accessor: 'marcas' },
+    { Header:'Apresentação', accessor: 'apresentacao' },
+    { Header:'Mínimo (R$)', accessor: 'minino' },
+    { Header:'Máximo (R$)', accessor: 'maximo' },
+    { Header:'Médio (R$)', accessor: 'mediano' }
+  ],[]);*/
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    pageOptions,
+    page,
+    state: { pageIndex, pageSize },
+    gotoPage,
+    previousPage,
+ nextPage,
+  setPageSize,
+     canPreviousPage,
+     canNextPage,
+     setFilter,
+  } = useTable({
+    columns,
+    data,
+    initialState: {
+      pageSize: 5
+    }
+  }, useFilters, usePagination);
+
+  console.log(data);
+
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilterInput(value);
+    setFilter('substancia', value)
+  };
+
+  return (
+    <div className='w-full h-full'>
+    <input
+      value={filterInput}
+      onChange={handleFilterChange}
+      placeholder={'Buscar'}
+    />
+    <table {...getTableProps()} className='table-fixed w-full'>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()} className='px-4 py-2'>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {page.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+                        className='border px-4 py-4 h-1/5'
+                      >
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                })}
+              </tr>
+            )
+          })}
+      </tbody>
+      <tfoot>
+      <div className="inline-flex">
+        <button
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          onClick={() => previousPage()} disabled={!canPreviousPage}
+        >
+          <span className="material-icons-outlined">arrow_back</span>
+        </button>
+        <button
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+          onClick={() => nextPage()} disabled={!canNextPage}
+        >
+          <span className="material-icons-outlined">arrow_forward</span>
+        </button>
+      </div>
+       <div>
+        <p>{pageIndex + 1} de {pageOptions.length}</p>
+       </div>
+       <div>Go to page:</div>
+       <input
+         type="number"
+         defaultValue={pageIndex + 1 || 1}
+         onChange={e => {
+           const page = e.target.value ? Number(e.target.value) - 1 : 0
+           gotoPage(page)
+         }}
+       />
+       <select
+         value={pageSize}
+         onChange={e => {
+           setPageSize(Number(e.target.value))
+         }}
+       >
+         {pageSizeOptions.map(pageSize => (
+           <option key={pageSize} value={pageSize}>
+             Show {pageSize}
+           </option>
+         ))}
+       </select>
+      </tfoot>
+    </table>
+    </div>
+  );
+/*  const tableInstance = useTable({ columns, data })
 
   return (
     <div className="w-screen h-screen">
@@ -74,7 +219,8 @@ function App (){
         }}
       />       
     </div>
-  );
+  );*/
+
 }
 
 export default App;
